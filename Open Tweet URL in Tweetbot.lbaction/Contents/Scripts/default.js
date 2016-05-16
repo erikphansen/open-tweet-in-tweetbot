d3620f5b1fb3e5b78c58d5b5a615e72d30e3dd5a
Erik P. Hansen <eph@erikphansen.com>
2016-05-16 10:32:33 -0700

    Fix so that the action works even if TweetBot isn't already running

--- Open Tweet URL in Tweetbot.lbaction/Contents/Scripts/default.js
+++ Open Tweet URL in Tweetbot.lbaction/Contents/Scripts/default.js
@@ -1,16 +1,18 @@
 // LaunchBar Action Script
 
 function run(argument) {
-  var regex = /^https?:\/\/(www.)?twitter.com\//
   // make sure that we passed in something, ANYTHING, to the action
   if (argument == undefined) {
-    LaunchBar.alert('No argument was passed to the action');
+    LaunchBar.alert('Please enter a Twitter URL');
   }
+
   // make sure we passed in a valid Twitter URL
+  var regex = /^https?:\/\/(www.)?twitter.com\//
   if (argument.match(regex) != null && argument.match(regex).index === 0) {
     // replace the `https://www.twitter.com/` with `tweetbot://`
     argument = argument.replace(regex, "tweetbot://")
     // now just tell LaunchBar to open the tweetbot URL
+    var tweetbotIsOpen = LaunchBar.executeAppleScriptFile("open-tweetbot.scpt")
     LaunchBar.openURL(argument);
   } else {
     LaunchBar.alert('It appears that\n`' + argument + '`\nis not a valid Twitter URL')
